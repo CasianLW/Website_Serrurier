5188cd275997a8750abdd2cf1000068642b4b2d7
CasianLW <ciorbcasian@gmail.com>
2023-05-24 20:09:56 +0200

    Update & fix nav &other

--- components/nav/burger-component.tsx
+++ components/nav/burger-component.tsx
@@ -1,35 +1,44 @@
-import { FC } from 'react'
-import BurgerBarComponent from './burger-bar-component'
-
-import { useNavSettingsContext } from '@/context/nav-settings-context'
+import { FC } from "react";
+import BurgerBarComponent from "./burger-bar-component";
+import { useNavSettingsContext } from "@/context/nav-settings-context";
 
 const BurgerComponent: FC = () => {
-  const { setNavIsClosed, navIsClosed, closing, setClosing } = useNavSettingsContext()
+  const { setNavIsClosed, navIsClosed, closing, setClosing } =
+    useNavSettingsContext();
 
   const handleClick = () => {
-    console.log(closing)
-    if (closing) return
-    setNavIsClosed(!navIsClosed)
+    console.log(closing);
+    if (closing) return;
+    setNavIsClosed(!navIsClosed);
     if (navIsClosed) {
-      setClosing(true)
+      setClosing(true);
       window.setTimeout(() => {
-        setClosing(false)
-      }, 800)
+        setClosing(false);
+      }, 800);
     }
-  }
+  };
 
   return (
     <div
-      className="absolute top-5 right-[5vw] grid grid-cols-[auto_22px] items-center gap-2 text-[17px] lg:hidden"
+      className="absolute top-10 left-[5vw] grid grid-cols-[auto_22px] items-center gap-2 text-[17px] lg:hidden"
       onClick={handleClick}
     >
-      <span>{navIsClosed ? 'Menu' : 'Fermer'}</span>
-      <div className="grid gap-[5px]">
-        <BurgerBarComponent transform={navIsClosed ? null : 'translate-y-[3.5px] rotate-45'} />
-        <BurgerBarComponent transform={navIsClosed ? 'w-3/4 ml-auto' : 'w-full -translate-y-[3.5px] -rotate-45'} />
+      {/* <span>{navIsClosed ? "Menu" : "Fermer"}</span> */}
+      <div className="grid gap-[6px] w-[40px]">
+        <BurgerBarComponent
+          transform={navIsClosed ? null : "translate-y-[3.5px] rotate-45"}
+        />
+        <BurgerBarComponent
+          transform={navIsClosed ? "w-full " : "w-full hidden"}
+        />
+        <BurgerBarComponent
+          transform={
+            navIsClosed ? "w-full " : "w-full -translate-y-[3.5px] -rotate-45"
+          }
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BurgerComponent
+export default BurgerComponent;
