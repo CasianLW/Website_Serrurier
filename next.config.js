78b1a2e5e3e585444313c5a7fd5ab902f7044d07
CasianLW <ciorbcasian@gmail.com>
2023-05-24 20:10:15 +0200

    footer import cities from csv or json document

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.csv$/,
+      use: [
+        {
+          loader: "csv-loader",
+          options: {
+            dynamicTyping: true,
+            header: true,
+            skipEmptyLines: true,
+          },
+        },
+      ],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
