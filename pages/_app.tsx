1d25efb1b76de89c9f50d32ca86af6fc103c7908
CasianLW <ciorbcasian@gmail.com>
2023-05-26 10:47:03 +0200

    maj favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,8 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="favicon.ico" />
+        {/* <link rel="icon" href="favicon.ico" /> */}
+        <link rel="icon" href="/favicon_securiclefs.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
